6898d9b3b0a8cfe7cbc303f9d2290986dd88f027
yongyi <reach.yongyixu@gmail.com>
2018-11-07 15:40:58 +0700

    layout

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 import './App.css';
+
+import Dashboard from './layouts/Dashboard/Dashboard'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Dashboard}/>
+          <Route exact path="/dashboard" component={Dashboard}/>
+        </Switch>
+      </Router>
     );
   }
 }
---
+++ src/layouts/Dashboard/Dashboard.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Dashboard.scss';
+
+class Dashboard extends Component {
+    render(){
+        return (
+            <div>
+                This is the Dashboard
+            </div>
+        )
+    }
+}
+
+export default Dashboard;
